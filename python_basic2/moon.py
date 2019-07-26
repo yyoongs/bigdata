b4f0bfed1d9fad41c4554c4472a88e2d3717b988
yyoongs <48539539+yyoongs@users.noreply.github.com>
2019-07-26 11:03:55 +0900

    Add files via upload

---
+++ python_basic2/moon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 1
+
+
+# In[2]:
+
+
+_b = 2
+
+
+# In[3]:
+
+
+def x():
+    return 1
+
+
+# In[ ]:
+
+
+def _y():
+    return 2
+
---
+++ python_basic2/star.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 1
+
+
+# In[5]:
+
+
+_b = 2
+
+
+# In[ ]:
+
+
+c = '문근영'
+
+
+# In[3]:
+
+
+def x():
+    return 1
+
+
+# In[6]:
+
+
+def _y():
+    return 2
+
+
+# In[7]:
+
+
+__all__ = ['a']
+
+
+# In[ ]:
+
+
+
+
